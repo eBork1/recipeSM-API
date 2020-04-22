6302d0fc36c6ced0264386b992e6dde3c64b1518
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-04-22 11:38:19 -0400

    add routes

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/verifyuser', 'AuthenticationController@verify')->name('verify');
+
