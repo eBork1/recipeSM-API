0c016d58cf2145f30a769bbcfbc578f707287859
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-04-22 21:14:09 -0400

    initial commit

---
+++ app/Http/Controllers/PofileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PofileController extends Controller
+{
+    public function index (Request $request)
+    {
+        // Get user bio
+        // Get user profile pic url
+        // Get use recipes
+        // Get user community posts
+        // Get user feed posts
+
+
+
+    }
+}
