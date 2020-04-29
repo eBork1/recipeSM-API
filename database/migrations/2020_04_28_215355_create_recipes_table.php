4b7d87c6809b7420ced6f9a92c118e35a50fca2a
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-04-28 22:38:02 -0400

    initial commit - create recipe model

---
+++ database/migrations/2020_04_28_215355_create_recipes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text("title");
+            $table->foreignId("user_id");
+            $table->integer("difficulty")->nullable();
+            $table->longText("ingredients")->nullable();
+            $table->longText("body");
+            $table->text("image_links")->nullable();
+
+            // Foreign key constraints
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
