a17226178b25a95e584616765ce010c23e69d15e
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-06-24 01:06:24 -0400

    add create follow method

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -1,23 +1,31 @@
 <?php
 
-use App\Follow;
-
 namespace App\Http\Controllers;
+
+use App\User;
+use App\Follow;
+use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
     public function createFollow(Request $data)
     {
-        //
+        $followed_user_id = User::where('name', $data->followed_user)->first()->id;
+
+        Follow::create([
+            "user_id" => $data->user()->id,
+            "followed_user" => $followed_user_id,
+        ]);
+        return response("success");
     }
 
-    public function unfollow()
-    {
-        //
-    }
+    // public function unfollow()
+    // {
+    //     //
+    // }
 
-    public function getFollowerCount(Request $data)
-    {
+    // public function getFollowerCount(Request $data)
+    // {
 
-    }
+    // }
 }
